03ca9e6c727c5d03816402ada09ff607ce2c99bf
Pagola Hugo <hugo.pagola@ynov.com>
2025-03-24 16:21:16 +0100

    [FIX] Entity.java(fixing and separating collision entity and collision with walls to have a better handling collision overall)

--- src/main/java/fr/ynov/pacman/domain/entity/Entity.java
+++ src/main/java/fr/ynov/pacman/domain/entity/Entity.java
@@ -1,13 +1,14 @@
 package main.java.fr.ynov.pacman.domain.entity;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
-public abstract class Entity
-{
+public abstract class Entity {
     protected int x;
     protected int y;
     protected int speed;
     protected Direction direction;
+    protected final int size = 20;
 
     public Entity(int x, int y, int speed) {
         this.x = x;
@@ -20,18 +21,22 @@ public abstract class Entity
     public abstract void draw(Graphics g);
 
     public boolean isColliding(Entity other) {
-        return this.x == other.x && this.y == other.y;
+        return getBounds().intersects(other.getBounds());
     }
 
-    public int getX() {
-        return x;
+    public boolean isCollidingWithWall(Wall wall) {
+        return getBounds().intersects(wall.getBounds());
     }
 
-    public int getY() {
-        return y;
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, size, size);
     }
 
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public Direction getDirection() { return direction; }
+    public int getSpeed() { return speed; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setDirection(Direction direction) { this.direction = direction; }
 }
